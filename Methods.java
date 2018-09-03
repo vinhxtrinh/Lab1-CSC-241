7e3e75ca0da77287a5865f9614662b1a85d7a276
vinhxtrinh <34748123+vinhxtrinh@users.noreply.github.com>
2018-09-03 12:41:59 -0500

    Initial commit

---
+++ Methods.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.*;
+
+public class Methods {
+    public void readArray(File file)
+    {
+        Scanner fileInput;
+        File inFile = new File("Array.txt");
+        Object [][] a = new Object[7][10];
+    }
+}
